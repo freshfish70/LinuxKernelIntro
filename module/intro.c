3f91b7883c0c25baf6e8ba0ce4c227f63126260a
Christoffer A Træen <christoffer.andersen@live.com>
2020-01-26 18:38:05 +0100

    Update info

--- module/intro.c
+++ module/intro.c
@@ -1,26 +1,34 @@
 /* 
-	Team X Module introduction to kernel programming 
-	Christoffer
-	Nils-Jarle 
+	Team 7 Module - introduction to kernel programming 
+	Christoffer Andersen Træen
+	Nils-Jarle Haugen
+	Trygve Woldseth
 */
 
-/* Load the require library */
+/* Load the required libraries */
 #include <linux/module.h> /* Needed by all modules */
 #include <linux/kernel.h> /* Needed for KERN_ALERT */
 
+// Module information
 MODULE_LICENSE("GPL");				 // Module License
-MODULE_DESCRIPTION("Team X module"); // Module Description
-MODULE_AUTHOR("Team X");			 // Module authors
+MODULE_DESCRIPTION("Team 7 module"); // Module Description
+MODULE_AUTHOR("Team 7");			 // Module authors
 
-// Called when module is loaded into the kernel
+/*
+ * The init_module methos is executed when
+ * the module is loaded into the kernel. 
+ */
 int init_module(void)
 {
-	printk("Team X module loaded :D \n");
+	printk("Team 7 module loaded :D \n");
 	return 0;
 }
 
-// Run when module is unloaded from the kernel
+/*
+ * The cleanup_module method is executed when
+ * the module is unloaded from the kernel 
+ */
 void cleanup_module(void)
 {
-	printk(KERN_ALERT "Team X module unloaded...\n");
+	printk(KERN_ALERT "Team 7 module unloaded...\n");
 }
